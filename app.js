e2e280cd0de4aacfbe40901124c6eceb9d146365
mbeckdev <mbeckdev42@gmail.com>
2021-07-11 21:28:13 -0600

    Create board from array layout

--- app.js
+++ app.js
@@ -42,9 +42,31 @@ document.addEventListener('DOMContentLoaded', () => {
   ];
 
   // Legend
-  // 0 - dot
+  // 0 - pac-dot
   // 1 - wall
   // 2 - ghost-lair
   // 3 - power-pellet
   // 4 - empty
+
+  let squares = [];
+
+  // draw the grid and render it
+  function createBoard() {
+    for (let i = 0; i < layout.length; i++) {
+      const square = document.createElement('div');
+      grid.appendChild(square);
+      squares.push(square);
+
+      // add layout to the board
+      if (layout[i] === 0) {
+        squares[i].classList.add('pac-dot');
+      } else if (layout[i] === 1) {
+        squares[i].classList.add('wall');
+      } else if (layout[i] === 3) {
+        squares[i].classList.add('power-pellet');
+      }
+    }
+  }
+
+  createBoard();
 });
